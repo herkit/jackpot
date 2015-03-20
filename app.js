4aeec94374f6f9332d07f3b23b4f72ca0632d0a7
Henrik Grotle <henrik@pswin.com>
2015-03-20 13:55:12 +0100

    misc socketio

--- app.js
+++ app.js
@@ -1,27 +1,62 @@
 var express = require('express.io'),
     exphbs  = require('express-handlebars'),
-    Game = require('./lib/game');
+    Game = require('./lib/game'),
+    merge = require('merge');
 
 var app = express();
 app.http().io();
 
 // Setup your sessions, just like normal.
 app.use(express.cookieParser())
-app.use(express.session({secret: 'monkey'}))
+app.use(express.session({secret: 'jackpot'}))
 
-app
-  .io
+app.io
   .route('ready', function(req) {
-    req.io.join(req.data)
-    req.io.room(req.data).broadcast('announce', {
-        message: 'New client in the ' + req.data + ' room. '
-    })
+
   });
+
+
 app.io
   .route('join', function(req) {
+    console.log("socket-io: join(" + req.data.gameid + ")");
+    var game = getGame(req.data.gameid);
 
+    var joinMessage = { name: req.data.name || "Player " + (game.state.players.length + 1), id: req.session.id };
+    console.log(joinMessage);
+
+    game.addPlayer(joinMessage, function(err, state) {
+      console.log(err);
+      console.log(state);
+      if (err)
+        req.io.room(state.id).broadcast("error", err);
+      else {
+        console.log("socket-id: join " + state.id);
+        req.io.broadcast("gamestate", state);
+      }
+    });
   });
-
+app.io
+  .route('observe', function(req) {
+    //req.io.join(req.data.gameid);
+  });  
+app.io
+  .route('roll', function(req) {
+    console.log("socket-io: roll(" + req.data.gameid + ")");
+    var game = getGame(req.data.gameid);
+    if (game.state.currentplayer.id === req.session.id)
+      game.rollDice(emitState(req));
+    else {
+      req.io.broadcast("error", { message: "It is not your turn" });
+    }
+  });
+app.io
+  .route('select', function(req) {
+    var game = getGame(req.data.gameid);
+    if (game.state.currentplayer.id === req.session.id)
+      game.select(req.data.digit, emitState(req));
+    else
+      req.io.broadcast("error", { message: "It is not your turn" });
+  });  
 
 
 
@@ -29,20 +64,43 @@ var games = {};
 
 var getGame = function(gameId) {
   if (!games[gameId]) {
-    games[gameId] = new Game(gameId, { name: "Player 1", win: 0, loss: 0 });
+    games[gameId] = new Game(gameId);
   }
   return games[gameId];
 }
 
-var outputState = function(res) {
+var getGameList = function() {
+  var gamelist = [];
+  for(id in games) {
+    var game = games[id];
+    gamelist.push({ id: id, players: game.state.players.length, mode: game.state.mode });
+  }
+  return gamelist;
+}
+
+var outputState = function(req, res) {
   return function(err, state) {
     if (err) {
       res.json(err, 403);
     } else {
-      res.json(state);
+      console.log(state);
+      res.json(merge.recursive(true, state, { you: req.session.id }));
     }
   }
 };
+
+var emitState = function(req) {
+  console.log("prepare emitState, " + req);
+  return function(err, state) {
+    console.log("emitState");
+    if (err) {
+      req.io.room(state.id).broadcast("error", err);
+    } else {
+      req.io.room(state.id).broadcast("gamestate", state);
+    }
+  }
+};
+
 
 app.set('port', (process.env.PORT || 3000))
 
@@ -56,29 +114,41 @@ app.get('/', function (req, res) {
   res.render('home');
 });
 
+app.get("/game", function(req, res) {
+  res.json(getGameList());
+});
+
 app.get("/game/:game", function(req, res) {
   res.render('game', getGame(req.params.game).state);
 });
 
 app.get("/game/:game/state", function(req, res) {
-  res.json(getGame(req.params.game));
+  outputState(req, res)(null, getGame(req.params.game).state)
 });
 
 app.get("/game/:game/roll", function(req, res) {
-  res.setHeader("Cache-Control", "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0')");
-  getGame(req.params.game).rollDice(outputState(res));
+  var game = getGame(req.params.game);
+  if (game.state.currentplayer.id === req.session.id)
+    game.rollDice(outputState(req, res));
+  else
+    res.json({ message: "It is not your turn" }, 403);
 });
 
 app.get("/game/:game/select/:number", function(req, res) {
-  getGame(req.params.game).select(parseInt(req.params.number), outputState(res));
+  var game = getGame(req.params.game);
+  if (game.state.currentplayer.id === req.session.id)
+    game.select(parseInt(req.params.number), outputState(req, res));
+  else
+    res.json({ message: "It is not your turn" }, 403);
 });
 
 app.get("/game/:game/join", function(req, res) {
-  getGame(req.params.game).addPlayer({ name: "Henrik" }, outputState(res));
+  var game = getGame(req.params.game);
+  game.addPlayer({ name: "Player " + (game.state.players.length + 1), id: req.session.id }, outputState(req, res));
 });
 
 app.get("/game/:game/next", function(req, res) {
-  getGame(req.params.game).nextPlayer(outputState(res));
+  getGame(req.params.game).nextPlayer(outputState(req, res));
 });
 
 app.listen(app.get('port'), function() {
--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,50 +1,51 @@
-var jackpotApp = angular.module('jackpotApp', []);
-
-io = io.connect();
+var jackpotApp = angular.module('jackpotApp', [
+  'jackpotApp.services',
+  'btford.socket-io'
+])
 
 jackpotApp.config(function ($interpolateProvider) {
   $interpolateProvider.startSymbol('{[{');
   $interpolateProvider.endSymbol('}]}');
 });
 
-jackpotApp.controller('jackpotGameCtrl', ['$scope', '$http', function ($scope, $http) {
+jackpotApp.controller('jackpotGameCtrl', ['$scope', 'socket', function ($scope, socket) {
   letters = ["-", "J", "A", "C", "K", "P", "O", "T", "-"];
 
-  $http.get("./state").
+  /*$http.get("./state").
   success(function(data, status, headers, config) {
+    $scope.state = data;
+  });*/
+
+  $scope.state = {};
+  $scope.state.digits = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+  $scope.state.dice = [1, 6];
+
+  socket.on("gamestate", function(data) {
+    console.log(data);
     $scope.state = data;
   });
 
-  /*$scope.state = {};
-  $scope.state.digits = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
-  $scope.state.dice = [1, 6];*/
-  $scope.gameid = "";
+  $scope.gameid = "asdf";
   $scope.myname = "";
   $scope.join = function() {
-
+    socket.emit("join", { gameid: $scope.gameid });
   };
   $scope.buttonsuccess = function(i) {
     return angular.isNumeric(i) ? false : true;
   };
-  $scope.ian = function (i) { console.log("ian " + i); return !isNaN(parseFloat(i)) && isFinite(i); };
-  $scope.nan = function (i) { console.log("nan " + i); return isNaN(parseFloat(i)); };
+  $scope.ian = function (i) { return !isNaN(parseFloat(i)) && isFinite(i); };
+  $scope.nan = function (i) { return isNaN(parseFloat(i)); };
   $scope.select = function(i) {
-    var idx = $scope.state.digits.indexOf(i);
-    if (idx >= 0) {
-      var diceTotal = $scope.state.dice.reduce(function(previousValue, currentValue, index, array) {
-        return previousValue + currentValue;
-      });
-      if ($scope.state.dice.indexOf(i) >= 0 || i == diceTotal) {
-        $scope.state.digits[idx] = letters[idx];
-      }
-    }
+    $socket.emit("select", { gameid: $scope.gameid, digit: i });
   };
   $scope.roll = function() {
-    var rolls = [];
+    console.log($scope);
+    $socket.emit("roll", { gameid: $scope.gameid });
+    /*var rolls = [];
     for(i in $scope.state.dice) {
       rolls.push(Math.floor(Math.random() * 20) + 1);
       $scope.state.dice[i] = Math.floor(Math.random() * 6) + 1;
 
-    }
+    }*/
   };
 }]);
